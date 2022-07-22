1e5a5f725483d0d2a820ca53017b6ad85bf857b4
Aparna <tiwari.aparna2899@gmail.com>
2022-07-22 23:33:43 +0530

    Assignment II

---
+++ block-BNaadi/routes/users.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+
+//create
+router.post('/', (req, res, next) => {
+  console.log(req.body);
+  User.create(req.body, (err, createdUser) => {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+});
+
+///read
+router.get('/', (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) return next(err);
+    res.render('listUsers', { users: users });
+  });
+});
+
+router.get('/:id', (req, res) => {
+  var id = req.params.id;
+  User.findById(id, (err, user) => {
+    res.render('singleUser', { user: user });
+  });
+});
+
+//update
+router.put('/:id', (req, res) => {
+  var id = req.params.id;
+  User.findByIdAndUpdate(id, req.body, (err, updateduser) => {
+    if (err) return next(err);
+    res.redirect('/users');
+  });
+});
+
+//delete
+router.delete('/:id', (req, res) => {
+  var id = req.params.id;
+  User.findByIdAndDelete(id, (err, user) => {
+    if (err) return next(err);
+    res.redirect('/users');
+  });
+});
+
+module.exports = router;
